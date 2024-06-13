1df5f3f7451022b56902021e7e2cd2b5cf5d87b5
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2024-06-13 15:00:18 +0200

    adding Drag and Drop

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 mousePosition;
+
+
+    private Vector3 GetMousePosition() 
+    {
+        return Camera.main.WorldToScreenPoint(transform.position);
+    }
+    
+    private void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition - GetMousePosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
