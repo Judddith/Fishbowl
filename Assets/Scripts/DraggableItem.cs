ae728d4e52b0b7041f4665eb7b6ccf168bb2e909
Leech1208 <nicolai.pfrengle@web.de>
2024-07-20 19:41:32 +0200

    camera lock when dragging items

--- Assets/Scripts/DraggableItem.cs
+++ Assets/Scripts/DraggableItem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using System;
+using Unity.VisualScripting;
 
 //Drag and drop handler
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
@@ -14,7 +16,6 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
     public GameObject bowl;
     public Camera playerCamera;
     public List<Item> itemsInBowl;
-    
 
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -24,6 +25,7 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
         transform.SetParent(transform.root);
         transform.SetAsLastSibling(); //display the dragged object over other objects
         image.raycastTarget = false; //makes object invisible for mouse input, object under mouse would be image otherwise
+        bowl.GetComponent<CameraRotator>().enabled = false; //disable camera movement while dragging
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -36,28 +38,26 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
     {
         Debug.Log("End drag");
 
+        bowl.GetComponent<CameraRotator>().enabled = true; //enable camera movement after dragging
         Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if(Physics.Raycast (ray, out hit)) {
+        if (Physics.Raycast(ray, out hit))
+        {
             Debug.Log("erste IF");
-            if (hit.collider.CompareTag("bowl")) {
+            if (hit.collider.CompareTag("bowl"))
+            {
                 Debug.Log("hit bowl!");
                 draggedObject.SetActive(true);
                 bowl.GetComponent<ItemsInBowl>().ItemsSpawned.Add(draggedObject); //adds active dragged item to item list
-
-                //image.raycastTarget = false;
-
-                // CanvasRenderer canvasRenderer;
-                // canvasRenderer = draggedObject.GetComponent<CanvasRenderer>();
-                // canvasRenderer.m_RaycastTarget = false;
             }
         }
 
         transform.SetParent(parentAfterDrag); //reset hierarchy after dropping
         image.raycastTarget = true; //makes object interactive with mouse again
-        
-        if (bowl.GetComponent<ItemsInBowl>().ItemsSpawned.Contains(draggedObject)) {
+
+        if (bowl.GetComponent<ItemsInBowl>().ItemsSpawned.Contains(draggedObject))
+        {
             image.raycastTarget = false;
         }
     }
